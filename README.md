2dce87e6a4a3c71eb62bc26facebd2ad12302c1d
Ruggero Colombo <da3a16@gmail.com>
2018-05-08 00:10:45 +0200

    flexible config

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
-# python port w/ secondary DNS enhance by da3a16
-	forked from https://github.com/xzer/wlanprofilemanager
+# python port w/ enhances by da3a16
+forked from https://github.com/xzer/wlanprofilemanager
+
+#Features
+
+Primary & optional secondary DNS setup
+
+Flexible config:
+	- static IP, static DNS
+	- IP via DHCP, static DNS
+	- Full DHCP
+
 # How to use
 
-- install python >= 3.6
-- clone this repository
-- add your own profile to profiles.config
-- register a task in task scheduler(control panel->administration tools->task scheduler)
-    - set wpm.bat as the operation of the task
-    - make sure the task will be executed by user "SYSTEM"
-    - define the trigger as following:
-        - start at: event
-        - basic, log: Microsoft-Windows-WLAN-AutoConfig/Operational
-        - source: WLAN-AutoConfig
-        - event id: 
+install python >= 3.6
+clone this repository
+add your own profile to profiles.config
+register a task in task scheduler(control panel->administration tools->task scheduler)
+	- set wpm.bat as the operation of the task
+	- make sure the task will be executed by user "SYSTEM"
+	- define the trigger as following:
+		- start at: event
+		- basic, log: Microsoft-Windows-WLAN-AutoConfig/Operational
+		- source: WLAN-AutoConfig
--- wpm.py
+++ wpm.py
@@ -3,10 +3,20 @@ from os import popen, system
 def enableProfile(intfName, ssid, profile):
     system('netsh interface ip set address name=\"{}\" static {} {} {}'.format(intfName, profile['ip'], profile['mask'], profile['gateway']))
     system('netsh interface ip set dnsserver name=\"{}\" static {} primary validate=no'.format(intfName, profile['dns']))
-    system('netsh interface ip add dns name=\"{}\" addr={} index=2 validate=no'.format(intfName, profile['dns2']))
-def enableDHCP(intfName):
+    if 'dns2' in profile:
+        system('netsh interface ip add dns name=\"{}\" addr={} index=2 validate=no'.format(intfName, profile['dns2']))
+
+def enableDHCP(intfName, profile):
     system('netsh interface ip set address name=\"{}\" source=dhcp'.format(intfName))
-    system('netsh interface ip set dnsserver name=\"{}\" source=dhcp'.format(intfName))
+    if profile == False:
+        system('netsh interface ip set dnsserver name=\"{}\" source=dhcp'.format(intfName))
+    else:
+        system('netsh interface ip set dnsserver name=\"{}\" static {} primary validate=no'.format(intfName, profile['dns']))
+        if 'dns2' in profile:
+           system('netsh interface ip add dns name=\"{}\" addr={} index=2 validate=no'.format(intfName, profile['dns2']))
+
+
+
 system('chcp 437')
 stdout = popen('netsh wlan show interfaces').read().split('\n')
 for line in stdout:
@@ -14,8 +24,15 @@ for line in stdout:
         intfName = line.split(':')[1].strip()
     elif 'SSID' in line and 'BSSID' not in line:
         ssid = line.split(':')[1].strip()
+
 profiles = json.load(open('profiles.config', 'r'))
+
 if ssid in profiles:
-    enableProfile(intfName, ssid, profiles[ssid])
+    if 'ip' in profiles[ssid] and 'mask' in profiles[ssid] and 'gateway' in profiles[ssid]:
+        enableProfile(intfName, ssid, profiles[ssid])
+    elif 'dns' in profiles[ssid]:
+        enableDHCP(intfName, profiles[ssid])
+    else:
+        enableDHCP(intfName, False)
 else:
-    enableDHCP(intfName)
+    enableDHCP(intfName, False)
